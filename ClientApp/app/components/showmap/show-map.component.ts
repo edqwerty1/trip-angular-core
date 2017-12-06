7ce6a255901457c4d1d67f4ec055fb54e881e6a6
Ed Blacklee <ed.blacklee@gmail.com>
2017-12-06 21:07:00 +0000

    updated location tiles to bootstrap 4 cards

--- ClientApp/app/components/showmap/show-map.component.ts
+++ ClientApp/app/components/showmap/show-map.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'show-map',
     template: `
-    <button type="button" class="btn btn-default" (click)="showMaps = !showMaps">
+    <button type="button" class="btn btn-primary" (click)="showMaps = !showMaps">
     Show Map
 </button>
 <div *ngIf="showMaps" style="height:400px; width:50%;">
